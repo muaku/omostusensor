712a4fa30f952f6c6ffcb10c0d4ee6a50d4d05e2
Muaku <muaku12@gmail.com>
2018-05-06 11:53:00 +0900

    setup code

---
+++ index.js
@@ -0,0 +1,17 @@
+const mraa = require('mraa'),
+var analogPin0 = new mraa.Aio(0);   //setup access analog input Analog pin #0 (A0)
+// var analogPin1 = new mraa.Aio(1);
+
+const vcc = 4.5;    // input volt
+const r1 = 10.0;    // [Kohm]
+const tempOffSet = 0;
+
+setInterval(readTemp, 1000)
+
+var readTemp = function() {
+    var a0 = analogPin0.read();     //read the value of the analog pin0
+    var v0 = (a0 * vcc) / 1024        // analog pin0 voltage
+    var temp = (v0 * 100 - tempOffSet).toFixed(2);
+    console.log("温度: ", temp)
+}
+
